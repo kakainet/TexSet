54c5dfe6041612602f516363c6860c83cd812e70
mvxxx <michal.tyrolski@gmail.com>
2020-05-07 14:51:28 +0200

    colors

--- ds/ds.py
+++ ds/ds.py
@@ -1,23 +1,41 @@
 import random
 import string
-colors = ['Apricot', 'Aquamarine', 'Bittersweet','Black','Blue','BlueGreen', 'BlueViolet', 'BrickRed', 'Brown','BurntOrange', 'CadetBlue' ,'CarnationPink', 'Cerulean','CornflowerBlue', 'Cyan', 'Dandelion', 'DarkOrchid', 'Emerald','ForestGreen','Fuchsia', 'Goldenrod','Gray','Green','GreenYellow','JungleGreen','Lavender','LimeGreen','Magenta', 'Mahogany', 'Maroon', 'Melon', 'MidnightBlue', 'Mulberry','NavyBlue', 'OliveGreen', 'Orange' ,'OrangeRed', 'Orchid', 'Peach'	,'Periwinkle' ,'PineGreen','Plum', 'ProcessBlue', 'Purple', 'RawSienna', 'Red', 'RedOrange', 'RedViolet', 'Rhodamine','RoyalBlue','RoyalPurple','RubineRed','Salmon','SeaGreen','Sepia','SkyBlue','SpringGreen','Tan','TealBlue','Thistle','Turquoise','Violet','VioletRed','White','WildStrawberry','Yellow','YellowGreen','YellowOrange']
+colors = ['Apricot', 'Aquamarine', 'Bittersweet','Black','Blue','BlueGreen', 'BlueViolet', 'BrickRed', 'Brown','BurntOrange', 'CadetBlue' ,'CarnationPink', 'Cerulean','CornflowerBlue', 'Cyan', 'Dandelion', 'DarkOrchid', 'Emerald','ForestGreen','Fuchsia', 'Goldenrod','Gray','Green','GreenYellow','JungleGreen','Lavender','LimeGreen','Magenta', 'Maroon', 'Melon', 'MidnightBlue', 'Mulberry','NavyBlue', 'OliveGreen', 'Orange' ,'OrangeRed', 'Orchid', 'Peach'	,'Periwinkle' ,'PineGreen','Plum', 'ProcessBlue', 'Purple', 'RawSienna', 'Red', 'RedOrange', 'RedViolet', 'Rhodamine','RoyalBlue','RoyalPurple','RubineRed','Salmon','SeaGreen','Sepia','SkyBlue','SpringGreen','Tan','TealBlue','Thistle','Turquoise','Violet','VioletRed','WildStrawberry','Yellow','YellowGreen','YellowOrange']
 
-binary = [R'\frac{0}{1}', '{0}+{1}', '{0}-{1}', R'{0} \cdot {1}']
-unary = [R'\sqrt{0}', R'\int{0}', R'\left({0}\right)']
+binary = [R'\frac{{ {0} }}{{ {1} }}', '{0}+{1}', '{0}-{1}', R'{0} \cdot {1}']
+unary = [R'\sqrt{{ {0} }}', R'\int {0} ', R'\left({0}\right)', R'f\left({0}\right)', R'F\left({0}\right)', R'G\left({0}\right)']
 op = binary+unary
-
+letters='xyabcd'
+symbols = string.digits+letters
 def color_expr(expr):
-    return fR'\color{random.choice(colors)}{expr}'
+    return '\color{{ {0} }}{{ {1} }}'.format(random.choice(colors), expr)
+
+def color_expr(expr, c):
+    return '\color{{ {0} }}{{ {1} }}'.format(c, expr)
+
+def randbool():
+    return bool(random.getrandbits(1))
+
+def atom():
+    if randbool():
+        return f'{random.choice(symbols)}{random.choice(letters)}'
+    else:
+        return f'{random.choice(symbols)}'
+
 
 def single(depth):
     if depth == 1:
-        return random.choice(string.digits)
+        return atom()
     return random.choice(op).format(single(depth-1), single(depth-1))
 
 def sample(k, d):
-    return [f'{color_expr(single(d))}+{color_expr(single(d))}' for _ in range(k)]
+    l = []
+    for _ in range(k):
+        c1, c2 = random.sample(colors ,2)
+        l.append(f'{color_expr(single(d), c1)}+{color_expr(single(d), c2)}')
+    return l
 
 
 if __name__ == "__main__":
-    for expr in sample(30, 2)
-        print(expr)
+    for expr in sample(20, 3):
+        print('$$ '+expr + ' $$')
