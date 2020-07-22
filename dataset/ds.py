a4297f8396ef317039f62136fee8ada310df9a6a
Michał Tyrolski <michal.tyrolski@gmail.com>
2020-07-22 14:00:25 -0700

    dataset (#1)
    * ds
    
    * colors
    
    * more complex
    
    * sample
    
    * boxes for single items
    
    * full automat
    
    * code quality
    
    * arg, generators etc
    
    * dataset

---
+++ dataset/ds.py
@@ -0,0 +1,45 @@
+import random
+import string
+import argparse
+
+colors = ['Apricot', 'Aquamarine', 'Bittersweet','Black','Blue','BlueGreen', 'BlueViolet', 'BrickRed', 'Brown','BurntOrange', 'CadetBlue' ,'CarnationPink', 'Cerulean','CornflowerBlue', 'Cyan', 'Dandelion', 'DarkOrchid', 'Emerald','ForestGreen','Fuchsia', 'Goldenrod','Gray','Green','GreenYellow','JungleGreen','Lavender','LimeGreen','Magenta', 'Maroon', 'Melon', 'MidnightBlue', 'Mulberry','NavyBlue', 'OliveGreen', 'Orange' ,'OrangeRed', 'Orchid', 'Peach'	,'Periwinkle' ,'PineGreen','Plum', 'ProcessBlue', 'Purple', 'RawSienna', 'Red', 'RedOrange', 'RedViolet', 'Rhodamine','RoyalBlue','RoyalPurple','RubineRed','Salmon','SeaGreen','Sepia','SkyBlue','SpringGreen','Tan','TealBlue','Thistle','Turquoise','Violet','VioletRed','WildStrawberry','Yellow','YellowGreen','YellowOrange']
+
+binary = [R'\frac{{ {0} }}{{ {1} }}', '{0}+{1}', '{0}-{1}', R'{0} \cdot {1}']
+unary = [R'\sqrt{{ {0} }}', R'\int {0} ', R'\left({0}\right)', R'f\left({0}\right)', R'F\left({0}\right)', R'G\left({0}\right)']
+op = binary+unary
+letters='xyabcd'
+symbols = string.digits+letters
+def color_expr(expr):
+    return '\color{{ {0} }}{{ {1} }}'.format(random.choice(colors), expr)
+
+def color_expr(expr, c):
+    return '\color{{ {0} }}{{ {1} }}'.format(c, expr)
+
+def randbool(f):
+    return random.randint(0,100) < f * 100
+
+def atom():
+    if randbool(0.5):
+        return f'{random.choice(symbols)}{random.choice(letters)}'
+    else:
+        return f'{random.choice(symbols)}'
+
+def single(depth):
+    if randbool(0.3) or depth == 1:
+        return atom()
+    return random.choice(op).format(single(depth-1), single(depth-1))
+
+def sample(k, d):
+    l = []
+    for _ in range(k):
+        c1, c2 = 'red','blue'
+        yield random.choice(binary).format(color_expr(single(d), c1), color_expr(single(d), c2))
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--samples', help='number of samples', required=True, type=int)
+    parser.add_argument('--max_depth', help='max allowed depth of LaTeX tree', required=True, type=int)
+    args = parser.parse_args()
+
+    for expr in sample(args.samples, args.max_depth):
+        print(expr)
---
+++ dataset/pudzianator.py
@@ -0,0 +1,68 @@
+import cv2
+import numpy as np
+import argparse
+import os
+
+def iswhite(p):
+    return p[0] > 180 and p[1] > 180 and p[2] > 180
+
+def avgcolor(img):
+    sum = [0,0,0]
+    ctr = 0
+    eps=0
+    rows,cols, _ = img.shape
+    for i in range(eps, rows - eps):
+        for j in range(eps, cols - eps):
+            p = img[i,j]
+            if not iswhite(p):
+                for c in range(3):
+                    sum[c] = sum[c] + p[c]
+                ctr=ctr+1
+
+    if ctr == 0:
+        return [0,0,0]
+    return [sum[j]/ctr for j in range(3)]
+
+
+
+def process_img(in_dir, out_dir, name):
+    print(f'Processing {name}...')
+    image = cv2.imread(os.path.join(in_dir, name))
+    gray = 255 - cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    boxes = {}
+    for i in range(8):
+        boxes[i] = []
+    cnts = cv2.findContours(gray, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = cnts[0] if len(cnts) == 2 else cnts[1]
+    for k, c in enumerate(cnts):
+        x,y,w,h = cv2.boundingRect(c)
+        subimg = image[y:y+h, x:x+w]
+        c = avgcolor(subimg)
+        hashcode = sum([(c[j] > 220) * (2**j) for j in range(3)])
+        boxes[hashcode].append([x,y,x+w,y+h])
+
+    for k in boxes.keys():
+        if not boxes[k]:
+            continue
+        m = 0
+
+        perchannel = np.array(boxes[k]).transpose()
+        xmin, ymin = np.min(perchannel[0]),np.min(perchannel[1])
+        xmaks, ymaks = np.max(perchannel[2]),np.max(perchannel[3]) 
+        print(xmin,ymin,xmaks,ymaks)
+        color = list(np.random.random(size=3) * 256)
+        cv2.rectangle(image, (xmin, ymin), (xmaks, ymaks), color, 1)
+
+    cv2.imwrite(os.path.join(out_dir, name), image)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--in_dir', help='input directory', required=True, type=str)
+    parser.add_argument('--out_dir', help='output directory', required=True, type=str)
+    args = parser.parse_args()
+    input_names = os.listdir(args.in_dir)
+
+    for name in input_names:
+        process_img(args.in_dir, args.out_dir, name)
+
