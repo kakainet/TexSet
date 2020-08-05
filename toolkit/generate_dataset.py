ce9edc0cd33a5fc9a1b65d1425ead07fb6c672e3
mvxxx <michal.tyrolski@gmail.com>
2020-08-05 23:53:32 +0200

    toolkit black inputs

--- toolkit/generate_dataset.py
+++ toolkit/generate_dataset.py
@@ -2,18 +2,20 @@ import subprocess
 import argparse
 import json
 import shutil
+import os
 
 def load_config():
     with open(args.config, 'r+') as json_config:
         return json.load(json_config)
 
-
 def generate_inputs():
     for f in output_files:
-            subprocess.run(["ds.py", "--max_depth", cfg['max-depth'], "--samples", cfg["samples-in-part"]], stdout=f) 
+        subprocess.run(["python3", "dataset/ds.py", "--max_depth", str(cfg['max-depth']), "--samples", str(cfg["samples-in-part"])], stdout=f) 
 
 def copy_black_inputs():
-    pass
+    for j in range(cfg['parts']):
+        for c in ['0,0,1', '1,0,0']:
+            output_cp_files[j].write(read_files[j].read().replace(c, '0,0,0'))
 
 def generate_images():
     pass
@@ -27,9 +29,11 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     cfg = load_config()
-    output_files = [open(os.path.join('dataset/latex2image/src', f'input{j}.in'), 'w+') for j in range(cfg['parts'])]
 
-   
+    output_files = [open(os.path.join('dataset/latex2image/src', f'input{j}.in'), 'w+') for j in range(cfg['parts'])]
+    output_cp_files = [open(os.path.join('dataset/latex2image/src', f'input{j}_black.in'), 'w+') for j in range(cfg['parts'])]
+    read_files = [open(os.path.join('dataset/latex2image/src', f'input{j}.in'), 'r') for j in range(cfg['parts'])]
+
     generate_inputs()
     copy_black_inputs()
     generate_images()
