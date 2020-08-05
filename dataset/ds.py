2632b85e4ba68f105681c370904efb9bc17ad015
mvxxx <michal.tyrolski@gmail.com>
2020-08-05 22:32:54 +0200

    dataset forward

--- dataset/ds.py
+++ dataset/ds.py
@@ -2,18 +2,22 @@ import random
 import string
 import argparse
 
-colors = ['Apricot', 'Aquamarine', 'Bittersweet','Black','Blue','BlueGreen', 'BlueViolet', 'BrickRed', 'Brown','BurntOrange', 'CadetBlue' ,'CarnationPink', 'Cerulean','CornflowerBlue', 'Cyan', 'Dandelion', 'DarkOrchid', 'Emerald','ForestGreen','Fuchsia', 'Goldenrod','Gray','Green','GreenYellow','JungleGreen','Lavender','LimeGreen','Magenta', 'Maroon', 'Melon', 'MidnightBlue', 'Mulberry','NavyBlue', 'OliveGreen', 'Orange' ,'OrangeRed', 'Orchid', 'Peach'	,'Periwinkle' ,'PineGreen','Plum', 'ProcessBlue', 'Purple', 'RawSienna', 'Red', 'RedOrange', 'RedViolet', 'Rhodamine','RoyalBlue','RoyalPurple','RubineRed','Salmon','SeaGreen','Sepia','SkyBlue','SpringGreen','Tan','TealBlue','Thistle','Turquoise','Violet','VioletRed','WildStrawberry','Yellow','YellowGreen','YellowOrange']
+def color_wrap(r,g,b,expr):
+    return fR'\\color[rgb]{{ {r}, {g}, {b} }}'
+
+def mul():
+    return '\cdot'
 
 binary = [R'\frac{{ {0} }}{{ {1} }}', '{0}+{1}', '{0}-{1}', R'{0} \cdot {1}']
 unary = [R'\sqrt{{ {0} }}', R'\int {0} ', R'\left({0}\right)', R'f\left({0}\right)', R'F\left({0}\right)', R'G\left({0}\right)']
+
+
 op = binary+unary
 letters='xyabcd'
 symbols = string.digits+letters
-def color_expr(expr):
-    return '\color{{ {0} }}{{ {1} }}'.format(random.choice(colors), expr)
 
 def color_expr(expr, c):
-    return '\color{{ {0} }}{{ {1} }}'.format(c, expr)
+    return '{{\color[rgb]{{ {0} }} {1} }}'.format(c, expr)
 
 def randbool(f):
     return random.randint(0,100) < f * 100
@@ -32,7 +36,7 @@ def single(depth):
 def sample(k, d):
     l = []
     for _ in range(k):
-        c1, c2 = 'red','blue'
+        c1, c2 = '1,0,0','0,0,1'
         yield random.choice(binary).format(color_expr(single(d), c1), color_expr(single(d), c2))
 
 if __name__ == "__main__":
