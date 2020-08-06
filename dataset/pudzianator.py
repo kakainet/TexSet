c86dad4f41782da689b8e192b926e5cba0d45eb1
Michał Tyrolski <michal.tyrolski@gmail.com>
2020-08-06 16:29:15 -0700

    Pipeline (#4)
    * toolkit prep
    
    * toolkit black inputs
    
    * generator decorators
    
    * toolkit - generating
    
    * black&color generating
    
    * full pipeline

--- dataset/pudzianator.py
+++ dataset/pudzianator.py
@@ -31,7 +31,6 @@ def avgcolor(img):
     return [sum[j]/ctr for j in range(3)]
 
 def process_img(in_dir, out_dir, name):
-    print(f'Processing {name}...')
     image = cv2.imread(os.path.join(in_dir, name))
     gray = 255 - cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     boxes = {}
@@ -45,7 +44,7 @@ def process_img(in_dir, out_dir, name):
         c = avgcolor(subimg)
         hashcode = sum([(c[j] > 220) * (2**j) for j in range(3)])
         boxes[hashcode].append([x,y,x+w,y+h])
-
+    bboxes=[]
     for k in boxes.keys():
         if not boxes[k]:
             continue
@@ -54,22 +53,26 @@ def process_img(in_dir, out_dir, name):
         perchannel = np.array(boxes[k]).transpose()
         xmin, ymin = np.min(perchannel[0]),np.min(perchannel[1])
         xmaks, ymaks = np.max(perchannel[2]),np.max(perchannel[3]) 
-        annotations.append({'name': name, 'bbox': [xmaks, ymaks, xmaks-xmin, ymaks-ymin]})
+        bboxes.append([xmaks, ymaks, xmaks-xmin, ymaks-ymin])
         color = list(np.random.random(size=3) * 256)
         cv2.rectangle(image, (xmin, ymin), (xmaks, ymaks), color, 1)
+    annotations.append({'name': name, 'bbox': bboxes})
 
     cv2.imwrite(os.path.join(out_dir, name), image)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--in_dir', help='input directory', required=True, type=str)
-    parser.add_argument('--out_dir', help='output directory', required=True, type=str)
+    parser.add_argument('--in-dir', help='input directory', required=True, type=str)
+    parser.add_argument('--out-dir', help='output directory', required=True, type=str)
     parser.add_argument('--save', help='save file', required=False, type=str)
 
     args = parser.parse_args()
     input_names = os.listdir(args.in_dir)
-    
+
+    if not os.path.isfile(args.out_dir):
+        os.mkdir(args.out_dir)
+
     annotations = []
 
     for name in input_names:
@@ -79,5 +82,5 @@ if __name__ == "__main__":
     if not args.save:
         print(json_output)
     else:
-        with open(args.save, 'r+') as savefile:
+        with open(args.save, 'w+') as savefile:
             savefile.write(json_output)
---
+++ toolkit/generate_dataset.py
@@ -0,0 +1,87 @@
+import subprocess
+import argparse
+import json
+import shutil
+import os
+import glob
+import signal
+import sys
+
+def signal_handler(sig, frame):
+    print('Aborted')
+    os.chdir(owd)
+    sys.exit(0)
+
+def dump_func_name(func):
+    def echo_func(*func_args, **func_kwargs):
+        print('Start func: {}'.format(func.__name__))
+        return func(*func_args, **func_kwargs)
+    return echo_func
+
+@dump_func_name
+def clean_old():
+    shutil.rmtree(result_dir)
+    os.mkdir(result_dir)
+
+@dump_func_name    
+def load_config():
+    with open(args.config, 'r+') as json_config:
+        return json.load(json_config)
+
+@dump_func_name  
+def generate_inputs():
+    for f in output_files:
+        subprocess.run(["python3", "dataset/ds.py", "--max_depth", str(cfg['max-depth']), "--samples", str(cfg["samples-in-part"])], stdout=f) 
+        
+@dump_func_name  
+def copy_black_inputs():
+    for j in range(cfg['parts']):
+        output_cp_files[j].write(read_files[j].read().replace('0,0,1', '0,0,0').replace('1,0,0', '0,0,0'))
+    for j in range(cfg['parts']):
+        output_cp_files[j].close()
+
+@dump_func_name  
+def generate_images():
+    os.chdir('dataset/latex2image/src')
+    colorfull = glob.glob('*.in')
+    black = glob.glob('*.in.black')
+    subprocess.run(['bash', 'set.sh', *colorfull])
+    os.rename('output','output_color')
+    os.rename('labels.txt', 'itl_labels.txt')
+    subprocess.run(['bash', 'set.sh', *black])
+    os.rename('output','output_black')
+    os.remove('labels.txt')
+    for f in black+colorfull:
+        os.remove(f)
+
+@dump_func_name
+def generate_bbox():
+    shutil.move('output_color', os.path.join(owd, result_dir)) 
+    shutil.move('output_black', os.path.join(owd, result_dir))
+    shutil.move('itl_labels.txt', os.path.join(owd, result_dir))
+    
+    os.chdir(owd)
+
+    subprocess.run(['python3', 'dataset/pudzianator.py', '--in-dir', os.path.join(result_dir, 'output_color'), '--out-dir', os.path.join(result_dir, 'output_bbox'), '--save', os.path.join(result_dir, 'annotations.json')])
+
+
+if __name__ == "__main__":
+    signal.signal(signal.SIGINT, signal_handler)
+    owd = os.getcwd()
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config', help='config file', required=True, type=str)
+
+    args = parser.parse_args()
+    cfg = load_config()
+    result_dir = 'output'
+
+    output_files = [open(os.path.join('dataset/latex2image/src', f'input{j}.in'), 'w+') for j in range(cfg['parts'])]
+    output_cp_files = [open(os.path.join('dataset/latex2image/src', f'input{j}.in.black'), 'w+') for j in range(cfg['parts'])]
+    read_files = [open(os.path.join('dataset/latex2image/src', f'input{j}.in'), 'r') for j in range(cfg['parts'])]
+
+    clean_old()
+    generate_inputs()
+    copy_black_inputs()
+    generate_images()
+    generate_bbox()
