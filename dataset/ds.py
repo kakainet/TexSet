d2edda4113c35721cab31e36cd2eb9179551d01f
syzymon <s.tworkowski@student.uw.edu.pl>
2020-08-13 21:08:41 +0200

    Reformat ds
    Deterministic coloring prototype

--- dataset/ds.py
+++ dataset/ds.py
@@ -3,27 +3,36 @@ import string
 import argparse
 import sys
 
-def color_wrap(r,g,b,expr):
+
+def color_wrap(r, g, b):
     return fR'\\color[rgb]{{ {r}, {g}, {b} }}'
 
+
 def mul():
-    return '\cdot'
+    return R'\cdot'
+
 
 def eprint(*args, **kwargs):
     print(*args, file=sys.stderr, **kwargs)
 
-binary = [R'\frac{{ {0} }}{{ {1} }}', '{0}+{1}', '{0}-{1}', R'{0} \cdot {1}']
-unary = [R'\sqrt{{ {0} }}', R'\int {0} ', R'\left({0}\right)', R'f\left({0}\right)', R'F\left({0}\right)', R'G\left({0}\right)']
 
-op = binary+unary
-letters='xyabcd'
-symbols = string.digits+letters
+inline_binary = {'{0}+{1}', '{0}-{1}', R'{0} \cdot {1}'}
+binary = inline_binary | {R'\frac{{ {0} }}{{ {1} }}'}
+unary = {R'\sqrt{{ {0} }}', R'\int {0} ', R'\left({0}\right)',
+         R'f\left({0}\right)', R'F\left({0}\right)', R'G\left({0}\right)'}
+
+ops = set(binary | unary)
+letters = 'xyabcd'
+symbols = string.digits + letters
+
 
 def color_expr(expr, c):
-    return '{{\color[rgb]{{ {0} }} {1} }}'.format(c, expr)
+    return R'{{\color[rgb]{{ {0} }} {1} }}'.format(c, expr)
+
 
 def randbool(f):
-    return random.randint(0,100) < f * 100
+    return random.randint(0, 100) < f * 100
+
 
 def atom():
     if randbool(0.5):
@@ -31,25 +40,34 @@ def atom():
     else:
         return f'{random.choice(symbols)}'
 
-def single(depth):
+
+def single(depth, forbidden_ops=None):
     if randbool(0.3) or depth == 1:
         return atom()
-    return random.choice(op).format(single(depth-1), single(depth-1))
+
+    allowed_ops = ops if not forbidden_ops else ops - forbidden_ops
+
+    return random.choice(tuple(allowed_ops)).format(single(depth - 1),
+                                                    single(depth - 1))
+
 
 def sample(k, d):
-    l = []
     for _ in range(k):
-        c1, c2 = '1,0,0','0,0,1'
-        s1, s2 = single(d), single(d)
-        bop = random.choice(binary)
-        yield bop.format(color_expr(s1, c1), color_expr(s2, c2)), bop.format(s1, s2)
+        c1, c2 = '1,0,0', '0,0,1'
+        s1, s2 = single(d, forbidden_ops=inline_binary), single(d)
+        bop = random.choice(tuple(binary))
+        yield bop.format(color_expr(s1, c1), color_expr(s2, c2)), bop.format(s1,
+                                                                             s2)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--samples', help='number of samples', required=True, type=int)
-    parser.add_argument('--max_depth', help='max allowed depth of LaTeX tree', required=True, type=int)
-    args = parser.parse_args()
+    parser.add_argument('--samples', help='number of samples', required=True,
+                        type=int)
+    parser.add_argument('--max_depth', help='max allowed depth of LaTeX tree',
+                        required=True, type=int)
+    cmd_args = parser.parse_args()
 
-    for expr, epxr_no_color in sample(args.samples, args.max_depth):
+    for expr, expr_no_color in sample(cmd_args.samples, cmd_args.max_depth):
         print(expr)
-        eprint(epxr_no_color)
+        eprint(expr_no_color)
