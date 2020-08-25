0de1380fd314e77cb11dc2729b8f73c7abf75163
Michał Tyrolski <michal.tyrolski@gmail.com>
2020-08-25 23:40:04 +0200

    transform stage (#14)

--- dataset/bbox_gen.py
+++ dataset/bbox_gen.py
@@ -4,31 +4,35 @@ import argparse
 import os
 import json
 
+
 def iswhite(p):
     return p[0] > 180 and p[1] > 180 and p[2] > 180
 
+
 def convert(o):
-    if isinstance(o, np.int64): 
-        return int(o)  
+    if isinstance(o, np.int64):
+        return int(o)
     else:
         return o
 
+
 def avgcolor(img):
-    sum = [0,0,0]
+    sum = [0, 0, 0]
     ctr = 0
-    eps=0
-    rows,cols, _ = img.shape
+    eps = 0
+    rows, cols, _ = img.shape
     for i in range(eps, rows - eps):
         for j in range(eps, cols - eps):
-            p = img[i,j]
+            p = img[i, j]
             if not iswhite(p):
                 for c in range(3):
                     sum[c] = sum[c] + p[c]
-                ctr=ctr+1
+                ctr = ctr+1
 
     if ctr == 0:
-        return [0,0,0]
+        return [0, 0, 0]
     return [sum[j]/ctr for j in range(3)]
+
 
 def process_img(in_dir, out_dir, name):
     image = cv2.imread(os.path.join(in_dir, name))
@@ -39,32 +43,35 @@ def process_img(in_dir, out_dir, name):
     cnts = cv2.findContours(gray, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     cnts = cnts[0] if len(cnts) == 2 else cnts[1]
     for k, c in enumerate(cnts):
-        x,y,w,h = cv2.boundingRect(c)
+        x, y, w, h = cv2.boundingRect(c)
         subimg = image[y:y+h, x:x+w]
         c = avgcolor(subimg)
         hashcode = sum([(c[j] > 220) * (2**j) for j in range(3)])
-        boxes[hashcode].append([x,y,x+w,y+h])
-    bboxes=[]
+        boxes[hashcode].append([x, y, x+w, y+h])
+    bboxes = []
     for k in boxes.keys():
         if not boxes[k]:
             continue
         m = 0
 
         perchannel = np.array(boxes[k]).transpose()
-        xmin, ymin = np.min(perchannel[0]),np.min(perchannel[1])
-        xmaks, ymaks = np.max(perchannel[2]),np.max(perchannel[3]) 
+        xmin, ymin = np.min(perchannel[0]), np.min(perchannel[1])
+        xmaks, ymaks = np.max(perchannel[2]), np.max(perchannel[3])
         bboxes.append([xmin, ymin, xmaks-xmin, ymaks-ymin])
         color = list(np.random.random(size=3) * 256)
         cv2.rectangle(image, (xmin, ymin), (xmaks, ymaks), color, 1)
-    annotations.append({'name': name, 'op': bboxes[0], 'exprs': sorted(bboxes[1:])})
+    annotations.append(
+        {'name': name, 'op': bboxes[0], 'exprs': sorted(bboxes[1:])})
 
     cv2.imwrite(os.path.join(out_dir, name), image)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--in-dir', help='input directory', required=True, type=str)
-    parser.add_argument('--out-dir', help='output directory', required=True, type=str)
+    parser.add_argument('--in-dir', help='input directory',
+                        required=True, type=str)
+    parser.add_argument('--out-dir', help='output directory',
+                        required=True, type=str)
     parser.add_argument('--save', help='save file', required=False, type=str)
 
     args = parser.parse_args()
@@ -78,7 +85,7 @@ if __name__ == "__main__":
     for name in input_names:
         process_img(args.in_dir, args.out_dir, name)
     json_output = json.dumps(annotations, default=convert)
-    
+
     if not args.save:
         print(json_output)
     else:
--- toolkit/transform_dataset.py
+++ toolkit/transform_dataset.py
@@ -4,29 +4,37 @@ import os
 import numpy as np
 import json
 
+
 def sort_annotations(jsonsource):
     def compare(entry):
-        get_idx = lambda entry : int(entry['name'][len('eq'):entry['name'].find('.')])
+        def get_idx(entry): return int(
+            entry['name'][len('eq'):entry['name'].find('.')])
         return get_idx(entry)
 
     return sorted(jsonsource, key=compare)
 
+
 def update_by_ratio(annotjson, idx, ratio):
     annotjson[idx]['op'] = [int(x * ratio) for x in annotjson[idx]['op']]
-    annotjson[idx]['exprs'] = [[int(x * ratio) for x in coords] for coords in annotjson[idx]['exprs']]
+    annotjson[idx]['exprs'] = [[int(x * ratio) for x in coords]
+                               for coords in annotjson[idx]['exprs']]
+
 
 def update_by_shift(annotjson, idx, shift, vertical):
     if vertical:
-        annotjson[idx]['op'] = [int(x  + shift * (i % 2 == 1 and i < 2)) for i, x in enumerate(annotjson[idx]['op'])]
-        annotjson[idx]['exprs'] = [[(x  + shift * (i%2 == 1 and i < 2)) for i, x in enumerate(coords)] for coords in annotjson[idx]['exprs']]
+        annotjson[idx]['op'] = [int(x + shift * (i % 2 == 1 and i < 2))
+                                for i, x in enumerate(annotjson[idx]['op'])]
+        annotjson[idx]['exprs'] = [[(x + shift * (i % 2 == 1 and i < 2))
+                                    for i, x in enumerate(coords)] for coords in annotjson[idx]['exprs']]
     else:
-        annotjson[idx]['op'] = [int(x  + shift * (i % 2 == 0 and i < 2)) for i, x in enumerate(annotjson[idx]['op'])]
-        annotjson[idx]['exprs'] = [[int(x  + shift * (i%2 == 0 and i < 2)) for i, x in enumerate(coords)] for coords in annotjson[idx]['exprs']]
+        annotjson[idx]['op'] = [int(x + shift * (i % 2 == 0 and i < 2))
+                                for i, x in enumerate(annotjson[idx]['op'])]
+        annotjson[idx]['exprs'] = [[int(x + shift * (i % 2 == 0 and i < 2))
+                                    for i, x in enumerate(coords)] for coords in annotjson[idx]['exprs']]
 
 
 def transform_dataset(input, output, dim):
-    print('start', annotjson)
-    idx=0
+    idx = 0
     for img_entry in os.scandir(input):
         if not os.path.isfile(img_entry.path):
             continue
@@ -36,7 +44,7 @@ def transform_dataset(input, output, dim):
         max_dim = max(height, width)
         ratio = dim/max_dim
         dsize = (round(width*ratio), round(height*ratio))
-        update_by_ratio(annotjson, idx, ratio)        
+        update_by_ratio(annotjson, idx, ratio)
         img = cv2.resize(img, dsize)
         assert(dim in img.shape)
         h, w, _ = img.shape
@@ -53,16 +61,21 @@ def transform_dataset(input, output, dim):
             img = np.vstack([strip, img])
             update_by_shift(annotjson, idx, shift, True)
 
-        cv2.imwrite(os.path.join(output, img_entry.name), cv2.resize(img, (dim, dim)))
-        idx+=1
-    print('end', annotjson)
+        cv2.imwrite(os.path.join(output, img_entry.name),
+                    cv2.resize(img, (dim, dim)))
+        idx += 1
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--input-dir', help='directory with dataset', required=True, type=str)
-    parser.add_argument('--output-dir', help='directory where output should be exported', required=True, type=str)
-    parser.add_argument('--dim', help='X/Y dim of output images', required=True, type=int)
-    parser.add_argument('--annotations', help='annotations file', required=True, type=str)
+    parser.add_argument(
+        '--input-dir', help='directory with dataset', required=True, type=str)
+    parser.add_argument(
+        '--output-dir', help='directory where output should be exported', required=True, type=str)
+    parser.add_argument(
+        '--dim', help='X/Y dim of output images', required=True, type=int)
+    parser.add_argument(
+        '--annotations', help='annotations file', required=True, type=str)
 
     FLAGS = parser.parse_args()
     annotations = FLAGS.annotations
