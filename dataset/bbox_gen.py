52b434c440b865b983f51a07f5cf7ebf253fd350
mvxxx <michal.tyrolski@gmail.com>
2020-10-05 22:08:32 +0200

    bbox gen blank

--- dataset/bbox_gen.py
+++ dataset/bbox_gen.py
@@ -17,40 +17,60 @@ def convert(o):
 
 
 def avgcolor(img):
-    sum = [0, 0, 0]
+    total = [0, 0, 0]
     ctr = 0
-    eps = 0
+    eps = int(0.15 * sum(img.shape)/len(img.shape))
+    print(f'eps={eps}')
     rows, cols, _ = img.shape
     for i in range(eps, rows - eps):
         for j in range(eps, cols - eps):
             p = img[i, j]
             if not iswhite(p):
                 for c in range(3):
-                    sum[c] = sum[c] + p[c]
+                    total[c] = total[c] + p[c]
                 ctr = ctr+1
 
     if ctr == 0:
         return [0, 0, 0]
-    return [sum[j]/ctr for j in range(3)]
+    return [total[j]/ctr for j in range(3)]
 
 
 def process_img(in_dir, out_dir, name):
+    print(f'Start {name}')
     image = cv2.imread(os.path.join(in_dir, name))
-    gray = 255 - cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    x,y,c = image.shape
+    assert(c==3)
+
+    # get all pixels which are gray
+    gray_detector = np.zeros((x,y,1))
+
+    for i in range(x):
+        for j in range(y):
+            b,g,r=[image[i,j,p] for p in range(3)]
+            gray_detector[i,j] = abs(b-g)+abs(b-r)+abs(g-r)
+
+    image2 = image.copy()
+    image2= np.where(gray_detector > 5, image2, [255,255,255])
+    cv2.imwrite(os.path.join(out_dir,'tmp', name), image2)
+    print(image.shape, image2.shape)
+    gray = 255 - cv2.cvtColor(image2, cv2.COLOR_BGR2GRAY)
+    exit(0)
     boxes = {}
     for i in range(8):
         boxes[i] = []
-    cnts = cv2.findContours(gray, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = cv2.findContours(gray, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_TC89_KCOS)
     cnts = cnts[0] if len(cnts) == 2 else cnts[1]
     for k, c in enumerate(cnts):
         x, y, w, h = cv2.boundingRect(c)
         subimg = image[y:y+h, x:x+w]
         c = avgcolor(subimg)
+        print(c)
         hashcode = sum([(c[j] > 220) * (2**j) for j in range(3)])
         boxes[hashcode].append([x, y, x+w, y+h])
     bboxes = []
-    for k in boxes.keys():
-        if not boxes[k]:
+    print(boxes)
+    for k in sorted(boxes.keys()):
+        if not boxes[k]: # colour not present or colour is black (== operator)
             continue
         m = 0
 
@@ -62,7 +82,7 @@ def process_img(in_dir, out_dir, name):
         cv2.rectangle(image, (xmin, ymin), (xmaks, ymaks), color, 1)
 
     annotations.append(
-        {'name': name, 'op': bboxes[0], 'exprs': sorted(bboxes[1:])})
+        {'name': name, 'op': '', 'exprs': sorted(bboxes)})
 
     cv2.imwrite(os.path.join(out_dir, name), image)
 
@@ -78,7 +98,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
     input_names = os.listdir(args.in_dir)
 
-    if not os.path.isfile(args.out_dir):
+    if not os.path.exists(args.out_dir):
         os.mkdir(args.out_dir)
 
     annotations = []
